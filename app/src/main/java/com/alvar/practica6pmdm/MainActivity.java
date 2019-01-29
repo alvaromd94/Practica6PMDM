f8697ae992ba86f9c4a6199dfc6fde4a23837ce0
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-01-29 21:22:40 +0100

    Crear menú de la Activity Principal

--- app/src/main/java/com/alvar/practica6pmdm/MainActivity.java
+++ app/src/main/java/com/alvar/practica6pmdm/MainActivity.java
@@ -3,9 +3,12 @@ package com.alvar.practica6pmdm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +18,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickPersona(View view1) {
-        Intent intent = new Intent (view1.getContext(), Contactos.class);
-        startActivityForResult(intent, 0);
+        Intent intent1 = new Intent(view1.getContext(), Contactos.class);
+        startActivityForResult(intent1, 0);
     }
 
     public void clickBloc(View view2) {
-        Intent intent = new Intent (view2.getContext(), Notepad.class);
-        startActivityForResult(intent, 0);
+        Intent intent2 = new Intent(view2.getContext(), Notepad.class);
+        startActivityForResult(intent2, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.Contactos:
+                Intent intent1 = new Intent(this, Contactos.class);
+                startActivity(intent1);
+                break;
+            case R.id.Notepad:
+                Intent intent2 = new Intent(this, Notepad.class);
+                startActivity(intent2);
+                break;
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
