26f81921056a0da54ad1c548548bce9450144822
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-01-29 20:04:34 +0100

    Crear Layout Principal

--- app/src/main/java/com/alvar/practica6pmdm/MainActivity.java
+++ app/src/main/java/com/alvar/practica6pmdm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alvar.practica6pmdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void clickPersona(View view1) {
+        Intent intent = new Intent (view1.getContext(), Contactos.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void clickBloc(View view2) {
+        Intent intent = new Intent (view2.getContext(), Notepad.class);
+        startActivityForResult(intent, 0);
     }
 }
