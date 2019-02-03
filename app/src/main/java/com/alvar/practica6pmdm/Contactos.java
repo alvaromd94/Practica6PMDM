91a44bf72807ebc0d6f6159951de15f85d88823f
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-02-03 12:09:31 +0100

    Probando Menu contextual de Activity Personas #4

--- app/src/main/java/com/alvar/practica6pmdm/Contactos.java
+++ app/src/main/java/com/alvar/practica6pmdm/Contactos.java
@@ -1,13 +1,65 @@
 package com.alvar.practica6pmdm;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CALL_PHONE;
 
 public class Contactos extends AppCompatActivity {
-
+    ImageView imageView, imageView2, imageView3, imageView4, imageView5, imageView6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactos);
+         imageView = findViewById(R.id.imageView);
+         imageView2 = findViewById(R.id.imageView2);
+         imageView3 = findViewById(R.id.imageView3);
+         imageView4 = findViewById(R.id.imageView4);
+         imageView5 = findViewById(R.id.imageView5);
+         imageView6 = findViewById(R.id.imageView6);
+
+
+        registerForContextMenu(imageView);
+        registerForContextMenu(imageView2);
+        registerForContextMenu(imageView3);
+        registerForContextMenu(imageView4);
+        registerForContextMenu(imageView5);
+        registerForContextMenu(imageView6);
+
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_contactos, menu);
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Llamar:
+                Toast.makeText(this, "UNO", Toast.LENGTH_SHORT).show();break;
+            case R.id.Correo:
+                Toast.makeText(this, "DOS", Toast.LENGTH_SHORT).show();break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/alvar/practica6pmdm/Edicion.java
@@ -0,0 +1,13 @@
+package com.alvar.practica6pmdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Edicion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edicion);
+    }
+}
