dff117b9d5454876c29499a68ff93e76010d26ab
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-02-04 11:42:52 +0100

    Generando métodos onClick de cada imagen #5

--- app/src/main/java/com/alvar/practica6pmdm/Edicion.java
+++ app/src/main/java/com/alvar/practica6pmdm/Edicion.java
@@ -2,6 +2,7 @@ package com.alvar.practica6pmdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Edicion extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class Edicion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion);
+    }
+
+    public void clickPersona1(View view) {
+    }
+
+    public void clickPersona2(View view) {
+    }
+
+    public void clickPersona3(View view) {
+    }
+
+    public void clickPersona4(View view) {
+    }
+
+    public void clickPersona5(View view) {
+    }
+
+    public void clickPersona6(View view) {
     }
 }
